d9c09cbc65c247d4761d7c80d5101982bc8d26cb
Franklim Porfirio <81085657+FranklimSilva@users.noreply.github.com>
2023-03-09 13:59:16 -0300

    Questão 5 - Valor Perfeito
    OBS:Por acidente, inverti a ordem das questões 4 e 5.

---
+++ ValorPerfeito/ValorPerfeito/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValorPerfeito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um numero para saber se ele é Perfeito:");
+            var num = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(num))
+            {
+                valorPerfeito(Convert.ToInt32(num));
+            }
+        }
+        public static void valorPerfeito(int num)
+        {
+            for(var i=1; i<num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("Valor é Perfeito!");
+                    Console.WriteLine("");
+                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                }
+                else
+                {
+                    Console.WriteLine("O numero possui resto! Ele não é Perfeito.");
+                    Console.WriteLine("");
+                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                }
+            }
+
+        }
+
+    }
+}
